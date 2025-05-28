42769728b74b11203200e8de19605d71675049c7
khyahahati <132439126+khyahahati@users.noreply.github.com>
2025-05-28 15:41:32 +0530

    DELETE request added

--- appointment-booking.py
+++ appointment-booking.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from datetime import datetime
 app = FastAPI()
@@ -10,7 +10,7 @@ class User(BaseModel):
     doctor_name: str
     appointment_date: datetime
     
-
+#Create an appointment 
 @app.post("/appointments")
 def create_appointment(user: User):
     appointment_id = len(appointments) + 1
@@ -24,7 +24,20 @@ def create_appointment(user: User):
     return{"message" : f"Appointment has been succesfully booked for {user.patient_name} with {user.doctor_name} on {user.appointment_date}",
            "appointment_id" : appointment_id}
 
-
+#View an appointment
 @app.get("/appointments")
 def view_appointments():
     return appointments    
+
+#Cancel an appointment
+@app.delete("/appointments/{appointment_id}")
+def cancel_appointment(appointment_id: int):
+    for i, appt in enumerate(appointments):
+        if appt["id"] == appointment_id:
+            cancelled_appointment = appointments.pop(i)
+            return {
+                "message" : f"The appointment for {cancelled_appointment['patient_name']} with {cancelled_appointment['doctor_name']} on {cancelled_appointment['appointment_date']} has been cancelled."
+            }
+    raise HTTPException(status_code=404, detail= "Appointment not found")
+        
+    
